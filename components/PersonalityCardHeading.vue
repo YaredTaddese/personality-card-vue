d7ceb63119fd7e80f0ead686019c674b4d15283f
Yared Taddese <taddeseyared@gmail.com>
2023-05-26 09:03:27 +0300

    Switch PersonalityCardHeading to vertical on sm

--- components/PersonalityCardHeading.vue
+++ components/PersonalityCardHeading.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="flex gap-6 md:gap-8">
-    <img
-      class="h-16 w-16 md:h-24 md:w-24 bg-gray-100 outline outline-1 outline-gray-300 rounded-full"
-      :src="avatar"
-    />
+  <div class="flex flex-col items-center md:flex-row gap-6 md:gap-8">
+    <img class="h-24 w-24 bg-gray-100 outline outline-1 outline-gray-300 rounded-full" :src="avatar" />
     <div class="flex flex-col justify-center">
-      <h3 class="text-xl md:text-2xl text-heading-black font-bold">
+      <h3 class="text-xl text-center md:text-2xl md:text-left text-heading-black font-bold">
         {{ firstName }} {{ lastName }}
       </h3>
-      <h4 class="text-1xl text-subtitle-gray">
+      <h4 class="text-1xl text-subtitle-gray text-center md:text-left">
         {{ age }} /
         {{ gender }}
       </h4>
