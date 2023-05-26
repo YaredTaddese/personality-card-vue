50009a34a80051b94618d2556eeda9a148bd4159
Yared Taddese <taddeseyared@gmail.com>
2023-05-26 09:39:48 +0300

    Add a loading spinner component

--- components/PersonalityCard.vue
+++ components/PersonalityCard.vue
@@ -1,54 +1,59 @@
 <template>
-  <div v-if="loading">Loading...</div>
-  <div v-else-if="!(selectedUser && userBrands)">Error: Fetch failed</div>
   <div
-    v-else
-    class="bg-white outline outline-1 outline-gray-200 rounded-3xl p-8 md:p-16 w-full md:w-144"
+    :class="`${
+      loading ? 'h-screen' : ''
+    } bg-white outline outline-1 outline-gray-200 rounded-3xl p-8 md:p-16 w-full md:w-144 `"
   >
-    <PersonalityCardHeading
-      :first-name="selectedUser.first_name"
-      :last-name="selectedUser.last_name"
-      :age="getUserAge(selectedUser.date_of_birth)"
-      :gender="selectedUser.gender"
-      :avatar="selectedUser.avatar"
-    />
-
-    <p class="pt-12 pb-8">
-      {{ selectedUser.bio }}
-    </p>
-
-    <!-- Details -->
-    <PersonalityCardDetails
-      :username="selectedUser.username"
-      :dob="selectedUser.date_of_birth.replaceAll('-', '/')"
-      :phone="
-        selectedUser.phone_number.replaceAll('.', '-').split('x')[0].trim()
-      "
-      :email="selectedUser.email"
-      :subscription="selectedUser.subscription.plan"
-      :address="getAddressString(selectedUser.address)"
-    />
-
-    <!-- Brands -->
-    <div class="py-4">
-      <StyledHeader>Brands</StyledHeader>
-      <div class="py-4 flex flex-wrap gap-2">
-        <ChipLabel
-          v-for="brand in userBrands"
-          :key="brand.id"
-          :text="brand.brand"
-        />
-      </div>
+    <div v-if="loading">
+      <LoadingSpinner />
     </div>
+    <div v-else-if="!(selectedUser && userBrands)">Error: Fetch failed</div>
+    <div v-else>
+      <PersonalityCardHeading
+        :first-name="selectedUser.first_name"
+        :last-name="selectedUser.last_name"
+        :age="getUserAge(selectedUser.date_of_birth)"
+        :gender="selectedUser.gender"
+        :avatar="selectedUser.avatar"
+      />
 
-    <!-- Regenerate -->
-    <div class="py-4 flex justify-center">
-      <button
-        class="rounded-md bg-primary-500 hover:bg-primary-400/80 transition-colors text-white font-bold px-3 py-1"
-        @click="fetchStudent"
-      >
-        Random Student
-      </button>
+      <p class="pt-12 pb-8">
+        {{ selectedUser.bio }}
+      </p>
+
+      <!-- Details -->
+      <PersonalityCardDetails
+        :username="selectedUser.username"
+        :dob="selectedUser.date_of_birth.replaceAll('-', '/')"
+        :phone="
+          selectedUser.phone_number.replaceAll('.', '-').split('x')[0].trim()
+        "
+        :email="selectedUser.email"
+        :subscription="selectedUser.subscription.plan"
+        :address="getAddressString(selectedUser.address)"
+      />
+
+      <!-- Brands -->
+      <div class="py-4">
+        <StyledHeader>Brands</StyledHeader>
+        <div class="py-4 flex flex-wrap gap-2">
+          <ChipLabel
+            v-for="brand in userBrands"
+            :key="brand.id"
+            :text="brand.brand"
+          />
+        </div>
+      </div>
+
+      <!-- Regenerate -->
+      <div class="py-4 flex justify-center">
+        <button
+          class="rounded-md bg-primary-500 hover:bg-primary-400/80 transition-colors text-white font-bold px-3 py-1"
+          @click="fetchStudent"
+        >
+          Random Student
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -60,13 +65,15 @@ import PersonalityCardHeading from './PersonalityCardHeading.vue'
 import PersonalityCardDetails from './PersonalityCardDetails.vue'
 import ChipLabel from '@/components/ChipLabel.vue'
 import StyledHeader from '@/components/StyledHeader.vue'
+import LoadingSpinner from '@/components/LoadingSpinner.vue'
 
 export default {
   name: 'PersonalityCard',
   components: {
     ChipLabel,
-    PersonalityCardHeading,
+    LoadingSpinner,
     PersonalityCardDetails,
+    PersonalityCardHeading,
     StyledHeader,
   },
   computed: {
