9c1c3fc3fca9d1a4871dca5de536359abc4873c7
Yared Taddese <taddeseyared@gmail.com>
2023-05-26 12:25:49 +0300

    Update package name and app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'vue-nuxt-tailwind',
+    title: 'Personality Card',
     htmlAttrs: {
       lang: 'en',
     },
